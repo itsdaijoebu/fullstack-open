4476657079ec55e46078cfbfb0d0f7b25f5b5f70
itsdaijoebu <joseph@josephchan.me>
2022-11-23 16:18:47 -0500

    complete courseinfo

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,24 +1,60 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
-function Hello(props) {
+const Header = (course) => {
+  return <h1>{course.name}</h1>;
+};
+const Part = (content) => {
   return (
     <>
-      <p>Hello {props.age} y/o {props.name}</p>
+      <p>
+        {content.part.name} {content.part.exercises}
+      </p>
     </>
-  )
-}
-
-function App() {
-  const name = 'pete'
-  const age = 3
-
+  );
+};
+const Content = (content) => {
   return (
     <>
-      <p>'sup</p>
-      <Hello name="george" age={32+1}/>
-      <Hello name={name} age={age}/>
+      <Part part={content.parts[0]} />
+      <Part part={content.parts[1]} />
+      <Part part={content.parts[2]} />
     </>
-  )
-}
+  );
+};
+const Total = (content) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {content.parts[0].exercises + content.parts[1].exercises + content.parts[2].exercises}
+    </p>
+  );
+};
 
-export default App
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+  return (
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
+export default App;
