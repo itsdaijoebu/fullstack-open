a2eba6034e56066df0693412a8d8e191bd7db91c
itsdaijoebu <joseph@josephchan.me>
2022-11-26 22:37:48 -0500

    countries 2.12-2.14

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,34 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function CountryInfo({ country }) {
+  const [temp, setTemp] = useState('')
+  const [wind, setWind] = useState('')
+  const [weatherIcon, setWeatherIcon] = useState('')
 
+  const lat = country.capitalInfo.latlng[0]
+  const lon = country.capitalInfo.latlng[1]
+  const weatherKey = import.meta.env.VITE_WEATHER_KEY
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=metric`
+  useEffect(() => {
+    axios
+    .get(weatherUrl)
+    .then(res => {
+      console.log(res.data.main)
+      setTemp(res.data.main.temp)
+      setWind(res.data.wind.speed)
+      setWeatherIcon(`https://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`)
+    })
+  }, [])
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>capital {country.capital}</p>
+      <p>area {country.area} km2</p>
+      <h4>languages:</h4>
+      <ul>
+        {Object.values(country.languages).map((lang) => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+      <img src={country.flags.svg} alt="flag" width="100" />
+      <h2>Weather in {country.capital}</h2>
+      <p>temperature {temp} Celcius</p>
+      <img src={weatherIcon} alt="" />
+      <p>wind {wind} m/s</p>
     </div>
-  )
+  );
 }
 
-export default App
+function Countries({ countries }) {
+  const showInfo = ({country}) => {
+    const id=`${country.cca3}-info`
+    document.getElementById(id).hidden = !document.getElementById(id).hidden;
+  };
+
+  if (countries.length > 10) {
+    return (
+      <section>
+        <p>Too many matches, specify another filter</p>
+      </section>
+    );
+  } else if (countries.length > 1) {
+    return (
+      <section>
+        {countries.map((country) => (
+          <div key={country.cca3}>
+            <p>
+              {country.name.common}{" "}
+              <button onClick={() => showInfo({country})}>show</button>
+            </p>
+            <div id={`${country.cca3}-info`} hidden>
+              <CountryInfo country={country}/>
+            </div>
+          </div>
+        ))}
+      </section>
+    );
+  } else if (countries.length === 1) {
+    return (
+      <section>
+        <CountryInfo country={countries[0]} />
+      </section>
+    );
+  }
+}
+
+function App() {
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      setCountries(res.data);
+    });
+  }, []);
+
+  const filterCountries = (e) => {
+    const filtered = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredCountries(filtered);
+  };
+
+  return (
+    <>
+      <label htmlFor="find-countries">find countries</label>
+      <input type="text" id="find-countries" onChange={filterCountries} />
+      <Countries countries={filteredCountries} />
+    </>
+  );
+}
+
+export default App;
