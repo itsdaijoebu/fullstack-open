0aa26287d46ec71cf35fdd4441d505f263fb5d8e
itsdaijoebu <joseph@josephchan.me>
2022-11-26 20:10:33 -0500

    finish phonebook, start countries

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import axios from 'axios'
 
 const People = ({ persons }) => {
   return (
@@ -73,17 +75,17 @@ const Form = ({
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
   const [filteredNames, setFilteredNames] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/persons').then(res => {
+      setPersons(res.data)
+    })
+  }, [])
 
   return (
     <div>
